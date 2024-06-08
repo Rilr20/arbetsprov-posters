f1abb996051b72cfea2f61cc7a5887be1d4effee
Rilr20 <nrrl99@gmail.com>
2024-06-08 21:46:59 +0200

    random button

--- admin/index.php
+++ admin/index.php
@@ -6,16 +6,25 @@
 $data = [];
 function ImageSearch($searchString, $perpage = 4): mixed
 {
+
     unset($_SESSION["searchinfo"]);
     if ($searchString == "") {
         $_SESSION["searchinfo"] = "Missing search string";
         return [];
     }
+    
     $curl = curl_init();
-    // $header = array();
-    // $search = "nature";
-    // $header[] = 'Authorization: xxjCiUcIHYdK5dqtEeqbJpf3b2dUmGrZLoi0ai4ueO9Nfkeoggmn6uB1';
-    curl_setopt($curl, CURLOPT_URL, "https://api.pexels.com/v1/search?query=" . urlencode($searchString) . "&per_page=$perpage");
+
+    if ($searchString == "POST_random") {
+
+        curl_setopt($curl, CURLOPT_URL,  "https://api.pexels.com/v1/curated?per_page=$perpage");
+        
+    } else {
+        echo "else";
+        curl_setopt($curl, CURLOPT_URL,  "https://api.pexels.com/v1/search?query=" . urlencode($searchString) . "&per_page=$perpage");
+
+    }
+
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_HTTPGET, true);
     curl_setopt($curl, CURLOPT_HTTPHEADER, array(
@@ -23,25 +32,22 @@ function ImageSearch($searchString, $perpage = 4): mixed
     ));
     $result = curl_exec($curl);
     curl_close($curl);
-    // print_r($result);
+
     $data = json_decode($result);
-    // var_dump($data);
+
 
     $params = [];
     //sort for original url
     foreach ($data->photos as $photo) {
-        # code...
+
         array_push($params, $photo->src->original);
     }
-    // var_dump($params);
+
 
     if (count($params) != 0) {
         $pdo = new PDO('mysql:host=localhost;dbname=posters', "root", "");
         $in = str_repeat("?,", count($params) - 1) . "?";
-        // $in = implode(',', array_fill(0, $perpage, '?'));
 
-        // var_dump("SELECT imageurl FROM search WHERE imageurl in ($in) AND reviewed=0");
-        // var_dump($params);
         $stmt = $pdo->prepare("SELECT imageurl FROM search WHERE imageurl in ($in)");
         $stmt->execute($params);
         $stmtres = $stmt->fetchAll();
@@ -56,41 +62,40 @@ function ImageSearch($searchString, $perpage = 4): mixed
             }
         }
 
-        // var_dump($stmtres);
+
     }
     // var_dump($data);
     if (count($data->photos) == 0) {
         $_SESSION["searchinfo"] = "No new images found";
     }
+    // var_dump($data->photos[0]->src);
+    echo '<pre>', var_dump($data->photos[0]->src), '</pre>';
     return $data;
 }
-// var_dump($data->photos[0]);
-// echo "<br>";
-// var_dump($data->photos[0]->src);
-// var_dump($data->photos->src);
 
-// var_dump($result);
-
-// "Authorization: xxjCiUcIHYdK5dqtEeqbJpf3b2dUmGrZLoi0ai4ueO9Nfkeoggmn6uB1" \"https://api.pexels.com/v1/search?query=nature&per_page=1"
 if (array_key_exists('search', $_POST)) {
     // var_dump($_POST);
     $search = $_POST["searchstring"];
     $data = ImageSearch($search);
     // var_dump($data);
 }
+if (array_key_exists('random', $_POST)) {
+    $data = ImageSearch("POST_random");
+}
 ?>
 <h1>Admin Page</h1>
 <form method="post">
     <input type="text" name="searchstring" id="searchstring">
     <button type="submit" name="search" value="">Search</button>
+    <button type="submit" name="random" value="">Random</button>
 </form>
 <form action="?page=process" method="POST">
-    <p <?php (isset($_SESSION["searchinfo"])) ? "hidden" : ""  ?>><?= (isset($_SESSION["searchinfo"])) ? $_SESSION["searchinfo"]  : ""?></p>
+    <p <?php (isset($_SESSION["searchinfo"])) ? "hidden" : ""  ?>><?= (isset($_SESSION["searchinfo"])) ? $_SESSION["searchinfo"]  : "" ?></p>
     <?php if (is_object($data)) { ?>
         <?php foreach ($data->photos as $key => $value) :  ?>
 
             <div>
-                <img src=<?= $value->src->tiny ?> alt=<?= $search ?>></img>
+                <img src=<?= $value->src->tiny ?> alt=<?= $search ?? "random" ?>></img>
             </div>
             <input id="yes-<?= $key ?>" type="radio" name="selection[<?= $key ?>]" value="1" required>
             <label for="yes-<?= $key ?>">yes</label>
