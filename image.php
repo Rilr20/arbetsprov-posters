a536ced36af0288bf43c37b6b735005ebe2b36cb
Rilr20 <nrrl99@gmail.com>
2024-06-10 21:50:22 +0200

    bug fixes if id doesn't exist

--- image.php
+++ image.php
@@ -2,17 +2,21 @@
 include './config.php';
 include './incl/doctype.php';
 include './incl/header.php';
-$poster = [];
-$pdo = new PDO($_ENV['mysql_dsn'], $_ENV['mysql_username'], $_ENV['mysql_password']);
-$id = $_GET['id'];
-$stmt = $pdo->prepare("SELECT p.id, p.description, p.title, p.header, p.image, GROUP_CONCAT(k.keyword SEPARATOR ', ') AS keywords FROM posters AS p JOIN poster_to_keywords AS pt ON p.id = pt.poster_id JOIN keywords AS k ON pt.keyword_id = k.id WHERE p.id = ? GROUP BY p.id;");
-$stmt->execute([$id]);
-$poster = $stmt->fetch();
+if (!empty($_GET['id'])) {
+    $poster = [];
+    $pdo = new PDO($_ENV['mysql_dsn'], $_ENV['mysql_username'], $_ENV['mysql_password']);
+
+    $id = $_GET['id'];
+    $stmt = $pdo->prepare("SELECT p.id, p.description, p.title, p.header, p.image, GROUP_CONCAT(k.keyword SEPARATOR ', ') AS keywords FROM posters AS p JOIN poster_to_keywords AS pt ON p.id = pt.poster_id JOIN keywords AS k ON pt.keyword_id = k.id WHERE p.id = ? GROUP BY p.id;");
+    $stmt->execute([$id]);
+    $poster = $stmt->fetch();
+}
 ?>
 <main>
     <h1 class="title">Poster</h1>
-    <h2 class="title"><?= $poster["title"] ?></h2>
     <p><a href="index.php">Index</a> Go Back to Index</p>
+    <?php if(!empty($poster)) : ?>
+        <h2 class="title"><?= $poster["title"] ?></h2>
     <div class="large-poster">
         <div class="large-poster-img">
             <img src="data:image/jpeg;base64,<?= base64_encode($poster['image']); ?>" alt="<?= $poster["title"] ?>">
@@ -24,6 +28,7 @@ $poster = $stmt->fetch();
                 <p class="poster-keyword-text"><?= $keyword ?></p>
             <?php endforeach; ?>
         </div>
+    <?php endif; ?>
 </main>
 
 <?php
