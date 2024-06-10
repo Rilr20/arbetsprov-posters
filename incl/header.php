4e8cd3c64efae219af67f2ab4a3c19ed4f549065
Rilr20 <nrrl99@gmail.com>
2024-06-10 21:00:23 +0200

    header styling

--- incl/header.php
+++ incl/header.php
@@ -1,14 +1,18 @@
 <?php
 ?>
-<div>
+<div class="wrapper-size">
     <form action="session.php" method="POST">
-        <fieldset>
+        <fieldset class="form-flex">
             <legend>Login</legend>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder="username" class="input">
+            <div>
 
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" class="input">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" placeholder="username" class="input">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="input">
+            </div>
 
             <button type="submit" name="login" class="button">Skicka</button>
             <p><?= empty($_SESSION["incorrect"]) != false ? "" : $_SESSION["incorrect"]; ?></p>
