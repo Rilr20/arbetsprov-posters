4c8ae5401961276389c36aa9a36536cecad9937c
Rilr20 <nrrl99@gmail.com>
2024-06-10 12:45:29 +0200

    removed var_dump

--- config.php
+++ config.php
@@ -3,7 +3,6 @@ require_once './vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-var_dump($_ENV['API_pexels']);
 $name = preg_replace("/[^a-z\d]/i", "", __DIR__);
 session_name($name);
 session_start();
