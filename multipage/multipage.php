d4e7f2af15e3cd0238e4ce93e04bf22100b91355
Rilr20 <nrrl99@gmail.com>
2024-06-11 00:15:58 +0200

    cleaned up file

--- multipage/multipage.php
+++ multipage/multipage.php
@@ -1,13 +1,4 @@
 <?php
-include(dirname(__FILE__) . '/../incl/doctype.php');
-?>
-<article class="table-report report">
-    <?php if ($page) : ?>
-        <?php require $page["file"] ?>
-    <?php else : ?>
-        <p>you have selected a page reference '<?= htmlentities($pageReference) ?>' that does not map to an actual page.</p>
-    <?php endif; ?>
-</article>
-<?php
-include(dirname(__FILE__) . '/../incl/footer.php');
-?>
+if ($page) {
+    require $page["file"];
+}
