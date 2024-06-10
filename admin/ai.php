a35adf852fa2c1304892ed1dcee7ff7dcdfdf2b7
Rilr20 <nrrl99@gmail.com>
2024-06-10 14:30:10 +0200

    Replaced Google Gemini with OpenAi

--- admin/ai.php
+++ admin/ai.php
@@ -1,7 +1,5 @@
 <?php
 
-use Gemini\Data\Blob;
-use Gemini\Enums\MimeType;
 
 $array = $_SESSION['ai'];
 // var_dump($array);
@@ -15,16 +13,35 @@ class Result
 }
 function generateData($url)
 {
-    $yourApiKey = $_ENV["API_gemini"];
-    $client = Gemini::client($yourApiKey);
+    $yourApiKey = $_ENV["API_openai"];#Todo: change variable
+    
+    $client = OpenAi::client($yourApiKey);
     $url = "$url?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280";
-    $binaryData = new Blob(MimeType::IMAGE_JPEG, base64_encode(file_get_contents(($url))));
+    // echo '<pre>', var_dump($client->chat()), '</pre>';
+
+    $result = $client->chat()->create([
+        'model' => 'gpt-4-vision-preview',
+        'messages' => [
+            [
+                'role' => 'user',
+                'content' => [
+                    ['type' => 'text', 'text' => 'based on the image only return this structure {"title":"","header":"","description":"","keywords": [],}'],
+                    ['type' => 'image_url', 'image_url' => "$url"],
+                ],
+            ]
+        ],
+        'max_tokens' => 900,
+    ]);
+
+    // return $result->choices[0]->message->content;
+    // $binaryData = new Blob(MimeType::IMAGE_JPEG, base64_encode(file_get_contents(($url))));
 
     // $result = $client->geminiProVision()->generateContent(['based on the image fill in this structure {    "title":"",    "header":"",    "description":"",    "keywords": [],}', $binaryData]);
     // $result->text();
 
-    // What it would look like
-    $result = new Result();
+    $data = $result->choices[0]->message->content;
+    //example
+    //```json { "title":"Monkey Looking at Reflection", "header":"A Curious Monkey with a Mirror", "description":"This photo captures a moment where a monkey is looking at its own reflection in a handheld mirror. The monkey appears to be intently examining itself, providing a sense of curiosity and self-awareness that is often attributed to primates.", "keywords": ["monkey", "reflection", "mirror", "self-awareness", "curiosity", "animal behavior", "primate", "nature"] } ```
 
     try {
         //code...
@@ -68,20 +85,38 @@ foreach ($_SESSION['ai'] as $value) {
 
 <h1>This is Ai Page Where you get Image Text</h1>
 <form action="?page=process" method="POST">
-    <?php foreach ($data as $key => $value) :  ?>
-        <div>
-            <?= $key ?>
-            <img src="<?= $value->url ?>?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280" alt="<?= $value->header ?>">
-            <label for="title">Title</label>
-            <input type="text" name="title" value="<?= $value->title ?>">
-            <label for="header">Header</label>
-            <input type="text" name="header" value="<?= $value->header ?>">
-            <label for="description">Description</label>
-            <textarea name="description" id="description" value=""><?= $value->description ?></textarea>
-            <label for="keywords">Keywords, comma separated</label>
-            <input type="text" name="keywords" value="<?= implode(', ', $value->keywords) ?>">
-        </div>
-    <?php endforeach; ?>
+    <div class="previews">
+        <?php foreach ($data as $key => $value) :  ?>
+            <div class="parent ">
+                <div class="div1">
+
+                    <img src="<?= $value->url ?>?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280" alt="<?= $value->header ?>">
+                </div>
+                <div class="div2 input-flex">
+
+                    <label for="title">Title</label>
+                    <input type="text" name="title" value="<?= $value->title ?>">
+                </div>
+                <div class="div3 input-flex">
+
+                    <label for="header">Header</label>
+                    <input type="text" name="header" value="<?= $value->header ?>">
+                </div>
+                <div class="div4 input-flex">
+
+                    <label for="description">Description</label>
+                    <textarea name="description" id="description" value=""><?= $value->description ?></textarea>
+                </div>
+                <div class="div5 input-flex">
+
+                    <label for="keywords">Keywords*</label>
+                    <input type="text" name="keywords" value="<?= implode(', ', $value->keywords) ?>">
+                    <p>*Comma separated</p>
+                </div>
+                <input type="hidden" name="url[<?= $key ?>]" value="<?= $_SESSION['ai'][$key] ?>">
+            </div>
+        <?php endforeach; ?>
+    </div>
     <input type="hidden" name="process" value="generation">
     <button type="submit">Validated</button>
 </form>
