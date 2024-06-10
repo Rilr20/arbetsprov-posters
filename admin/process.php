fb14fa67e270717b0b7bd8d118a8b9d104cc159a
Rilr20 <nrrl99@gmail.com>
2024-06-10 18:47:01 +0200

    checks if form is empty

--- admin/process.php
+++ admin/process.php
@@ -6,26 +6,27 @@ switch ($_POST['process']) {
          * Yes 1
          * No 0
          */
-        $pdo = new PDO('mysql:host=localhost;dbname=posters', "root", "");
+        if(empty($_POST['selection'])) {
+            var_dump($_POST['selection']);
+            $_SESSION["searchinfo"] = "Nothing to Send";
+            header("Location: admin.php");
+
+        }
+        $pdo = new PDO($_ENV['mysql_dsn'], $_ENV['mysql_username'], $_ENV['mysql_password']);
         $stmt_check = $pdo->prepare("SELECT imageurl from search where imageurl = ?");
         $stmt = $pdo->prepare('INSERT INTO search (reviewed, imageurl) VALUES(?, ?)');
         $_SESSION['ai'] = [];
-        
+
         for ($i = 0; $i < count($_POST["selection"]); $i++) {
-            // var_dump($_POST["url_".$i]);
 
             $params = [$_POST['url'][$i]];
-            // var_dump($_POST["selec"]);
-            // var_dump($stmt_check->execute($params));
+
             $stmt_check->execute($params);
-            // var_dump($stmt_check->execute());
+
             $result = $stmt_check->fetch();
-            // var_dump(empty($result));
+
             if (empty($result)) {
-                // echo "empty";
-                // var_dump($_POST["selection"][$i]);
-                // var_dump($_POST["url"][$i]);
-                // echo $_POST["url"][$i];
+
                 if ($_POST["selection"][$i] == 1) {
                     // var_dump($_POST["selection"][$i]);
                     array_push($_SESSION['ai'], $_POST["url"][$i]);
@@ -39,8 +40,55 @@ switch ($_POST['process']) {
         }
 
         break;
-    
-    default:
-        # code...
+    case 'generation':
+        for ($i = 0; $i < count($_POST['title']); $i++) {
+            $title = $_POST['title'][$i];
+            $header = $_POST['header'][$i];
+            $description = $_POST['description'][$i];
+            $url = $_POST['url'][$i];
+            $keywords = explode(",", $_POST['keywords'][$i]);
+
+            $pdo = new PDO($_ENV['mysql_dsn'], $_ENV['mysql_username'], $_ENV['mysql_password']);
+            $posterId = CreatePoster($pdo, $header, $title, $description, $url);
+            AddKeywords($pdo, $posterId, $keywords);
+            header("Location: admin.php");
+        }
         break;
+        // default:
+        //     # code...
+        //     header("Location: index.php");
+
+        //     break;
+}
+function CreatePoster($pdo, $header, $title, $description, $url): int
+{
+    $thumbnailUrl = "$url?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280";
+    $stmt = $pdo->prepare('INSERT INTO posters (title, description, header, image, thumbnail) VALUES (?,?,?,?,?)');
+    $image = file_get_contents("$url?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+    $thumbnail = file_get_contents($thumbnailUrl);
+    $stmt->execute([$title, $description, $header, $image, $thumbnail]);
+    return $pdo->lastInsertId();
+}
+function AddKeywords($pdo, $posterId, $keywords)
+{
+    $stmtSelect = $pdo->prepare('SELECT id, keyword FROM keywords WHERE keyword = ?');
+    $stmtCreate = $pdo->prepare('INSERT INTO keywords (keyword) VALUES (?)');
+    $stmtConnect = $pdo->prepare('INSERT INTO poster_to_keywords (poster_id, keyword_id)  VALUES (?,?)');
+    foreach ($keywords as $keyword) {
+        $stmtSelect->execute([$keyword]);
+        $result = $stmtSelect->fetch();
+        //check if keyword exists
+        if (empty($result)) {
+            //Create keyword if it doesn't
+            $stmtCreate->execute([$keyword]);
+            $keywordId = $pdo->lastInsertId();
+        } else {
+            // var_dump($result["id"]);
+            $keywordId = $result["id"];
+        }
+        //Get the keyword id, add it to poster_to_keywords
+        $stmtConnect->execute([$posterId, $keywordId]);
+    }
+
+
 }
